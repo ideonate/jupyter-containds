cfd0b30acaea3fb7882c0dd81d44a05abed69781
Adrien DELSALLE <ad.delsalle@gmail.com>
2020-08-26 17:15:10 +0200

    pre-fill new dashboard form use api changes to pre-fill name and path

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,32 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
   ILayoutRestorer
-} from "@jupyterlab/application";
+} from '@jupyterlab/application';
 
-import {
-  ICommandPalette,
-  ToolbarButton
-} from "@jupyterlab/apputils";
+import { ICommandPalette, ToolbarButton
+} from '@jupyterlab/apputils';
 
-import { ISettingRegistry } from "@jupyterlab/settingregistry";
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
-import { PageConfig } from "@jupyterlab/coreutils";
+import { PageConfig } from '@jupyterlab/coreutils';
 
-import { DocumentRegistry } from "@jupyterlab/docregistry";
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-import { IMainMenu } from "@jupyterlab/mainmenu";
+import { IMainMenu } from '@jupyterlab/mainmenu';
 
 import {
   INotebookTracker,
   NotebookPanel,
   INotebookModel
-} from "@jupyterlab/notebook";
+} from '@jupyterlab/notebook';
 
-import { CommandRegistry } from "@lumino/commands";
+import { CommandRegistry } from '@lumino/commands';
 
-import { ReadonlyJSONObject } from "@lumino/coreutils";
+import { ReadonlyJSONObject } from '@lumino/coreutils';
 
-import { IDisposable } from "@lumino/disposable";
+import { IDisposable } from '@lumino/disposable';
 
-import { Token } from "@lumino/coreutils";
+import { Token } from '@lumino/coreutils';
 
 const IContainDSToken = new Token<IContainDSToken>('jupyterlab-containds:IContainDSToken');
 
@@ -44,7 +42,7 @@ const CONTAINDS_ICON_CLASS = 'jp-MaterialIcon cds-dashboard-icon'
  * The command IDs used by the plugin.
  */
 export namespace CommandIDs {
-  export const containdsOpen = "notebook:open-with-containds";
+  export const containdsOpen = 'notebook:open-with-containds';
 }
 
 /**
@@ -65,16 +63,16 @@ class ContainDSOpenButton
    */
   createNew(panel: NotebookPanel): IDisposable {
     const button = new ToolbarButton({
-      className: "containdsOpen",
-      tooltip: "Deploy as a ContainDS Dashboard",
+      className: 'containdsOpen',
+      tooltip: 'Deploy as a ContainDS Dashboard',
       iconClass: CONTAINDS_ICON_CLASS,
-      iconLabel: "Dashboard",
+      iconLabel: 'Dashboard',
       onClick: () => {
         this._commands.execute(CommandIDs.containdsOpen);
       }
     });
-    panel.toolbar.insertAfter("cellType", "containdsOpen", button);
-    console.log("INSERTED containdsOpen");
+    panel.toolbar.insertAfter('cellType', 'containdsOpen', button);
+    console.log('INSERTED containdsOpen');
     return button;
   }
 
@@ -85,13 +83,14 @@ class ContainDSOpenButton
  * Initialization data for the jupyterlab-voila extension.
  */
 const extension: JupyterFrontEndPlugin<IContainDSToken> = {
-  id: "@ideonate/jupyter-containds:plugin",
+  id: '@ideonate/jupyter-containds:plugin',
   autoStart: true,
-  requires: [INotebookTracker],
+  requires: [JupyterFrontEnd.IPaths, INotebookTracker],
   optional: [ICommandPalette, ILayoutRestorer, IMainMenu, ISettingRegistry],
   provides: null,
   activate: (
     app: JupyterFrontEnd,
+    paths: JupyterFrontEnd.IPaths,
     notebooks: INotebookTracker,
     palette: ICommandPalette | null,
     restorer: ILayoutRestorer | null,
@@ -99,12 +98,24 @@ const extension: JupyterFrontEndPlugin<IContainDSToken> = {
     menu: IMainMenu | null,
     settingRegistry: ISettingRegistry | null
   ) => {
+    const hubHost = paths.urls.hubHost || '';
+    const hubPrefix = paths.urls.hubPrefix || '';
+
+    // Bail if not running on JupyterHub.
+    if (!hubPrefix) {
+      return;
+    } else {
+      console.debug('hub-extension: Found configuration ', {
+        hubHost: hubHost,
+        hubPrefix: hubPrefix
+      });
+    }
 
     const token = new ContainDSToken();
 
     function getCurrent(args: ReadonlyJSONObject): NotebookPanel | null {
       const widget = notebooks.currentWidget;
-      const activate = args["activate"] !== false;
+      const activate = args['activate'] !== false;
 
       if (activate && widget) {
         app.shell.activateById(widget.id);
@@ -122,14 +133,13 @@ const extension: JupyterFrontEndPlugin<IContainDSToken> = {
 
     function getDashboardUrl(path: string): string {
       const baseUrl = PageConfig.getBaseUrl();
-      return `${baseUrl}../../hub/dashboards-endpoint/${path}`;
+      return `${baseUrl}../../hub/dashboards-new/` + encodeURIComponent(path);
     }
-
 
     const { commands, docRegistry } = app;
 
     commands.addCommand(CommandIDs.containdsOpen, {
-      label: "Create as a ContainDS Dashboard in New Browser Tab",
+      label: 'Create as a ContainDS Dashboard in New Browser Tab',
       execute: async args => {
         const current = getCurrent(args);
         if (!current) {
@@ -143,10 +153,8 @@ const extension: JupyterFrontEndPlugin<IContainDSToken> = {
     });
 
     if (palette) {
-      const category = "Notebook Operations";
-      
+      const category = 'Notebook Operations';
       palette.addItem({ command: CommandIDs.containdsOpen, category });
-      
     }
 
     if (menu && menu.viewMenu) {
@@ -161,9 +169,7 @@ const extension: JupyterFrontEndPlugin<IContainDSToken> = {
     }
 
     const dashboardButton = new ContainDSOpenButton(commands);
-    docRegistry.addWidgetExtension("Notebook", dashboardButton);
-
-    console.log("DONE docRegistry.addWidgetExtension containdsOpen");
+    docRegistry.addWidgetExtension('Notebook', dashboardButton);
 
     return token;
   }
