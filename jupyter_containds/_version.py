e5253109ccf469c4b7b9d1fa0b1e8692e1f56e25
Frederic Collonval <frederic.collonval@ariadnext.com>
2021-04-07 15:07:22 +0000

    Bump version

--- jupyter_containds/_version.py
+++ jupyter_containds/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Ideonate.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, "2rc0")
+version_info = (0, 2, 2)
 __version__ = ".".join(map(str, version_info))
